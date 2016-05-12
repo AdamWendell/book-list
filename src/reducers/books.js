15298bde87d672b512078a6d6542017e7a96a211
Adam Wendell <adam@websmith.se>
2016-05-12 22:55:20 +0200

    Started a book site

---
+++ src/reducers/books.js
@@ -0,0 +1,9 @@
+export default function () {
+  return [
+    { title: 'Javascript: the good parts'},
+    { title: 'The art of war'},
+    { title: ''},
+    { title: 'Harry'},
+    { title: 'Haskell basics'},
+  ]
+}
